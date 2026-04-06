cf16563dfa8271c382c4e6dd8fdd801a03bf4568
root <root@WIN-L0OE7GKIMJ9.localdomain>
2026-04-06 14:42:42 +0900

    fix: register Vuetify components and directives

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,12 +5,16 @@ import '@mdi/font/css/materialdesignicons.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 const vuetify = createVuetify({
+  components,
+  directives,
   theme: {
     defaultTheme: 'autojudge',
     themes: {
